d89497c40955761383a37d9aac03b996dbf8c479
sumitkakshapati <me.summet@gmail.com>
2023-05-15 18:20:18 +0545

    add and subract feature added

---
+++ lib/calculator.dart
@@ -0,0 +1,17 @@
+class Calculator {
+  int add(int num1, int num2) {
+    return num1 + num2;
+  }
+
+  int subtract(int num1, int num2) {
+    return num1 - num2;
+  }
+
+  int multiplication(int num1, int num2) {
+    return num1 * num2;
+  }
+
+  double divide(int num1, int num2) {
+    return num1 / num2;
+  }
+}
---
+++ test/calculator_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test_example/calculator.dart';
+
+void main() {
+  group("Testing Calculator", () {
+    late Calculator calculator;
+
+    setUpAll(() {
+      calculator = Calculator();
+    });
+
+    test(
+      "Test Add function",
+      () {
+        final result = calculator.add(10, 20);
+        expect(result, 30);
+      },
+    );
+
+    test(
+      "Test Subtract function",
+      () {
+        final result = calculator.subtract(10, 20);
+        expect(result, -10);
+      },
+    );
+  });
+}
